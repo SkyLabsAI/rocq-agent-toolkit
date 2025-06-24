091dc64e5e229f6544adf904b4737cce639646f0
Rodolphe Lepigre <rodolphe.lepigre@skylabs-ai.com>
2025-06-24 16:38:59 +0200

    Basic diagnostic program based on the doc manager.

---
+++ rocq-doc-manager/bin/rocq_diagnose.ml
@@ -0,0 +1,63 @@
+(** Formatter with continuation. *)
+type ('a, 'b) koutfmt = ('a, Format.formatter, unit, unit, unit, 'b) format6
+
+(** [panic fmt ...] interrupts the program with [exit 1], after displaying the
+    error message specified by [fmt] (and subsequent arguments) to [stderr]. A
+    newline character is added to the message, and [stderr] is flushed. *)
+let panic : ('a,'b) koutfmt -> 'a = fun fmt ->
+  Format.kfprintf (fun _ -> exit 1) Format.err_formatter (fmt ^^ "\n%!")
+
+let or_panic : ('a, string) result -> 'a = fun res ->
+  match res with
+  | Error(s) -> panic "Error: %s" s
+  | Ok(v)    -> v
+
+let main : Document.t -> unit = fun state ->
+  or_panic (Document.load_file state);
+  let prev_data = ref None in
+  let rec loop () =
+    match Document.has_suffix state with
+    | false -> `Assoc([("status", `String("success"))])
+    | true  ->
+    match Document.run_step state with
+    | Error(loc,s) ->
+        let fields =
+          ("status" , `String("error")) ::
+          ("message", `String(s)) ::
+          ("loc"    , Document.loc_to_json loc) :: []
+        in
+        let fields =
+          match !prev_data with
+          | None -> fields
+          | Some(data) ->
+          match data.Document.open_subgoals with
+          | None -> fields
+          | Some(goals) -> fields @ [("goals", `String(goals))]
+        in
+        `Assoc(fields)
+    | Ok(None)     -> loop ()
+    | Ok(Some(v))  -> prev_data := Some(v); loop ()
+  in
+  let json = loop () in
+  let json = Yojson.Safe.pretty_to_string ~std:true json in
+  Printf.printf "%s%!" json
+
+(* We assume a single Rocq source file is passed last. *)
+let parse_args : argv:string array -> string list * string = fun ~argv ->
+  let argc = Array.length argv in
+  if argc < 2 then panic "Usage: %s [ROCQ ARGS] FILE.v" argv.(0);
+  let args =
+    let args = ref [] in
+    for i = argc - 2 downto 1 do args := argv.(i) :: !args done;
+    !args
+  in
+  let file = argv.(argc - 1) in
+  if not (Filename.check_suffix file ".v") then
+    panic "Error: a Rocq source file is expected as last argument.";
+  (args, file)
+
+let _ =
+  let (args, file) = parse_args ~argv:Sys.argv in
+  let state = Document.init ~args ~file in
+  try main state with
+  | Sys_error(s) -> panic "Error: %s" s
---
+++ rocq-doc-manager/tests/diagnose-error.t
@@ -0,0 +1,29 @@
+  $ export ROCQPATH="$DUNE_SOURCEROOT/_build/install/default/lib/coq/user-contrib"
+  $ export ROCQLIB="$DUNE_SOURCEROOT/_build/install/default/lib/coq"
+  $ export DUNE_CACHE=disabled
+
+  $ cat > test.v <<EOF
+  > Variant i := Test.
+  > Definition junk m n := m + n * n.
+  > Theorem test : forall x : nat, x = x.
+  > Proof.
+  >   intro x.
+  >   (*reflexivity.*)
+  > Qed.
+  > EOF
+
+  $ rocq-diagnose -Q . test.dir test.v
+  {
+    "status": "error",
+    "message": " (in proof test): Attempt to save an incomplete proof\n(there are remaining open goals).",
+    "loc": {
+      "fname": [ "ToplevelInput" ],
+      "line_nb": 1,
+      "bol_pos": 0,
+      "line_nb_last": 1,
+      "bol_pos_last": 0,
+      "bp": 128,
+      "ep": 132
+    },
+    "goals": "1 goal\n  \n  x : nat\n  ============================\n  x = x"
+  }
