b88458ad6f3fa257312518559f1928c0b5e3e93b
Rodolphe Lepigre <rodolphe.lepigre@skylabs-ai.com>
2025-06-24 18:50:53 +0200

    Force flush in wrapping utility.

--- ocaml-jsonrpc-tp/bin/tp_wrap.ml
+++ ocaml-jsonrpc-tp/bin/tp_wrap.ml
@@ -1,6 +1,6 @@
 let output_packet data =
   let len = String.length data + 1 in 
-  Printf.printf "Content-Length: %i\r\n\r\n%s\n" len data
+  Printf.printf "Content-Length: %i\r\n\r\n%s\n%!" len data
 
 let rec process_lines () =
   match In_channel.input_line stdin with None -> () | Some(line) ->
