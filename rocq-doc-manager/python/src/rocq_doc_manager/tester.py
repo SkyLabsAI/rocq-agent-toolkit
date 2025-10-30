7623c3a35422920786b820761341335ef25c120e
Gregory Malecha <gregory@skylabs-ai.com>
2025-10-30 13:34:50 -0400

    Moving the tests into poetry.

---
+++ rocq-doc-manager/python/src/rocq_doc_manager/tester.py
@@ -0,0 +1,12 @@
+import sys
+from rocq_doc_manager import RocqDocManager
+
+def cram_test1() -> None:
+    try:
+        dm = RocqDocManager([], sys.argv[1])
+        print(dm.request("non-existant", []))
+        print(dm.request("load_file", []))
+        print(dm.request("doc_suffix", []))
+        dm.quit()
+    except RocqDocManager.Error as e:
+        print(e)
---
+++ rocq-doc-manager/python/tests/cram-test.t/run.t
@@ -0,0 +1,4 @@
+  $ cram_test1 tests/test.v
+  RocqDocManager.Err(message='Method non-existant not found.', data=None)
+  RocqDocManager.Err(message='System error: "tests/test.v: No such file or directory"', data={'loc': None})
+  RocqDocManager.Resp(result=[])
