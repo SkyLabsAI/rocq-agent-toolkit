5238a7bc61925465476385d94ad1b6caeb4f8fa6
Gregory Malecha <gregory@skylabs-ai.com>
2025-11-20 15:30:38 -0500

    Plugin to extract the goal as Json.

---
+++ rocq-pipeline/src/rocq_pipeline/tracers/json_goal.py
@@ -0,0 +1,42 @@
+import json
+from typing import Any
+
+from rocq_doc_manager.rocq_doc_manager import RocqDocManager
+
+from .extractor import StateExtractor
+
+
+class JsonGoal(StateExtractor[list[Any]]):
+    _RAW_PATH = "skylabs_ai.extractors.goal_to_json.basic.goal_util"
+    _IRIS_PATH = "skylabs_ai.extractors.goal_to_json.iris.goal_util"
+
+    def _mod(self) -> str:
+        return JsonGoal._IRIS_PATH if self._iris else JsonGoal._RAW_PATH
+
+    def _tactic(self) -> str:
+        return f"all: {self._mod()}.goal_to_json."
+
+    def _check_iris(self, rdm: RocqDocManager) -> bool:
+        result = rdm.text_query("Locate iris.proofmode.environments.envs_entails.", 0)
+        assert not isinstance(result, RocqDocManager.Err)
+        return not result.startswith("No object")
+
+    def start_proof(self, rdm: RocqDocManager) -> None:
+        # Detect iris
+        self._iris = self._check_iris(rdm)
+
+        result = rdm.run_command(f"Require {self._mod()}.")
+        if isinstance(result, RocqDocManager.Err):
+            raise RuntimeError(f"Failed to initialize JsonGoal extractor: {result}")
+
+    def __call__(self, rdm: RocqDocManager) -> list[Any] | None:
+        result = rdm.text_query_all(self._tactic(), indices=None)
+        if isinstance(result, rdm.Err):
+            if "Init.Not_focussed" in result.message:
+                return []
+            print(f"got error, {result}")
+            return None
+        elif result == ['This subproof is complete, but there are some unfocused goals.\nTry unfocusing with "}".\n']:
+            return []
+        else:
+            return [json.loads(goal) for goal in result]
