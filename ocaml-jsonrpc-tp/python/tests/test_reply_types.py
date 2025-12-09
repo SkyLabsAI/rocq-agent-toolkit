66c8c346a4b9c25fd3dbd72398274c28633b7071
Jasper Haag <jasper@skylabs-ai.com>
2025-12-09 16:27:39 -0500

    feat(ocaml-jsonrpc-tp/python): make Err/Reply covariant; add a base Reply type, plus tests

---
+++ ocaml-jsonrpc-tp/python/tests/test_reply_types.py
@@ -0,0 +1,186 @@
+import re
+from collections.abc import Callable, Sequence
+
+import pytest
+
+from jsonrpc_tp import JsonRPCTP as API
+
+# NOTE: tests are tightly coupled to the underlying implementation
+# since we are patching dataclass to play nicely with covariant data,
+# cf. https://github.com/microsoft/pyright/discussions/11012
+
+
+class ReplyFixtures:
+    type _mk_Reply[T] = Callable[[T], API.Reply[T]]
+    type _mk_Err[T] = Callable[[str, T], API.Err[T]]
+    type _mk_Resp[T] = Callable[[T], API.Resp[T]]
+
+    @pytest.fixture(scope="session")
+    @staticmethod
+    def mk_Reply[T]() -> _mk_Reply[T]:
+        return lambda data: API.Reply(data=data)
+
+    @pytest.fixture(scope="session")
+    @staticmethod
+    def mk_Err[T]() -> _mk_Err[T]:
+        return lambda message, data: API.Err(message=message, data=data)
+
+    @pytest.fixture(scope="session")
+    @staticmethod
+    def mk_Resp[T]() -> _mk_Resp[T]:
+        return lambda result: API.Resp(result=result)
+
+    # Note: the rest of the fixtures use the default scope of function to
+    # ensure that get fresh instances
+
+    @pytest.fixture
+    @staticmethod
+    def Reply_None(mk_Reply: _mk_Reply[None]) -> API.Reply[None]:
+        return mk_Reply(None)
+
+    @pytest.fixture
+    @staticmethod
+    def Err_None(mk_Err: _mk_Err[None]) -> API.Err[None]:
+        return mk_Err("", None)
+
+    @pytest.fixture
+    @staticmethod
+    def Resp_None(mk_Resp: _mk_Resp[None]) -> API.Resp[None]:
+        return mk_Resp(None)
+
+
+class TestReplyErrResp(ReplyFixtures):
+    @pytest.mark.parametrize(
+        "reply_cls, expected_positional_args",
+        [
+            (API.Reply, ["data"]),
+            (API.Err, ["message", "data"]),
+            (API.Resp, ["result"]),
+        ]
+    )
+    @staticmethod
+    def test_required_positional_args(
+            reply_cls: type[API.Reply],
+            expected_positional_args: Sequence[str],
+    ) -> None:
+        with pytest.raises(TypeError) as exc_info:
+            # Note: explicitly testing ctor call that misses arguments
+            _reply = reply_cls()  # type: ignore
+        pluralization = "s" if 1 < len(expected_positional_args) else ""
+        assert exc_info.match(
+            re.escape(
+                " ".join(
+                    [
+                        f"{reply_cls.__qualname__}.__init__() missing",
+                        f"{len(expected_positional_args)} required positional",
+                        f"argument{pluralization}:",
+                        " and ".join(
+                            f"'{arg_nm}'" for arg_nm in expected_positional_args
+                        )
+                    ]
+                )
+            )
+        )
+
+    def test_bool_Reply_not_implemented(self, Reply_None: API.Reply[None]) -> None:
+        with pytest.raises(NotImplementedError):
+            bool(Reply_None)
+
+    def test_bool_Err(self, Err_None: API.Err[None]) -> None:
+        assert not bool(Err_None)
+
+    def test_bool_Resp(self, Resp_None: API.Resp[None]) -> None:
+        assert bool(Resp_None)
+
+    def test_eq_other_types(
+            self,
+            Reply_None: API.Reply[None],
+            Err_None: API.Err[None],
+            Resp_None: API.Resp[None],
+    ) -> None:
+        replies_None = [Reply_None, Err_None, Resp_None]
+
+        class SomeCls:
+            pass
+
+        class MyReply(API.Reply[None]):
+            pass
+
+        for i in range(len(replies_None)):
+            mk: object
+            for mk in [None, "", 0, [], {}, set(), float(0), SomeCls()]:
+                assert replies_None[i].__eq__(mk) is NotImplemented
+
+            eq = replies_None[i].__eq__(MyReply(None))
+            assert eq is not NotImplemented
+            assert not eq
+
+    def test_eq_None(
+            self,
+            Reply_None: API.Reply[None],
+            Err_None: API.Err[None],
+            Resp_None: API.Resp[None],
+    ) -> None:
+        replies_None = [Reply_None, Err_None, Resp_None]
+
+        for i in range(len(replies_None)):
+            for j in range(len(replies_None)):
+                a = replies_None[i]
+                b = replies_None[j]
+                a_eq_b = a.__eq__(b)
+                b_eq_a = b.__eq__(a)
+
+                assert a_eq_b is not NotImplemented
+                assert b_eq_a is not NotImplemented
+                assert isinstance(a_eq_b, bool)
+                assert isinstance(b_eq_a, bool)
+
+                if i == j:
+                    assert a_eq_b and b_eq_a
+                else:
+                    assert not (a_eq_b or b_eq_a)
+
+    def test_eq_Err_messages(
+            self,
+            mk_Err: ReplyFixtures._mk_Err[None]
+    ) -> None:
+        strs = ["", "Foo", "Bar", "Baz", "Qux"]
+        for i in range(len(strs)):
+            for j in range(len(strs)):
+                if i == j:
+                    assert mk_Err(strs[i], None) == mk_Err(strs[j], None)
+                else:
+                    assert mk_Err(strs[i], None) != mk_Err(strs[j], None)
+
+    def test_repr_roundtrip_None(
+            self,
+            Reply_None: API.Reply[None],
+            Err_None: API.Err[None],
+            Resp_None: API.Resp[None],
+    ) -> None:
+        replies_None = [Reply_None, Err_None, Resp_None]
+        for reply_None in replies_None:
+            assert (
+                eval(repr(reply_None), globals={"JsonRPCTP": API}) ==
+                reply_None
+            )
+
+    def test_covariant_Reply(
+            self,
+            mk_Reply: ReplyFixtures._mk_Reply,
+    ) -> None:
+        r1: API.Reply[None] = mk_Reply(None)
+        r2: API.Reply[str] = mk_Reply("FooBar")
+        r3: API.Reply[Exception] = mk_Reply(Exception())
+
+        assert r1 != r2
+        assert r1 != r3
+        assert r2 != r3
+
+        def rs() -> Sequence[API.Reply[None | str | Exception]]:
+            return [r1, r2, r3]
+
+        _r: API.Reply[None | str | Exception]
+        for r in rs():
+            _r = r
+            assert _r == r
