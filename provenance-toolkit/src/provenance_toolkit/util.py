8f35436e53b86ba40cd9b53c471ab8c63a13bd6e
Jasper Haag <jasper@skylabs-ai.com>
2025-12-16 15:14:08 -0500

    feat(provenance-toolkit/util): metaclass for namespace classes that contain aliases for deriving, but should not be derived

---
+++ provenance-toolkit/src/provenance_toolkit/util.py
@@ -0,0 +1,84 @@
+"""Utilities used by provenance-toolkit.
+
+Currently includes:
+- FinalTypeNamespace: metaclass for classes that bundle:
+  + related derivation targets (TypeAlias)
+  + generic utilities for any derivation target (staticmethod)
+"""
+from __future__ import annotations
+
+from typing import Any
+
+
+class FinalNamespaceMeta(type):
+    def __new__(
+        mcs: FinalNamespaceMeta,
+        name: str,
+        bases: list[type],
+        namespace: dict[str, Any],
+        *,
+        derive_from: dict[str, type] | None = None,
+        **kwargs: Any,
+    ):
+        """Prevent deriving from classes that use metaclass=FinalNamespaceMeta.
+
+        Arguments: beyond the default __new__ args (mcs, name, bases, namespace)
+          - derive_from (dict[str, type]): attr names / types that should be
+            derived from instead.
+
+        Raises:
+          - AttributeError: derive_from keys are missing from namespace
+          - ValueError: derive_from is empty, derive_from/namespace value mismatch
+          - TypeError: one of bases uses metaclass=FinalNamespaceMeta
+
+        Returns: self, ready for initialization via __init__
+        """
+
+        for b in bases:
+            if isinstance(b, FinalNamespaceMeta):
+                if derive_from is None:
+                    suggestion = ""
+                else:
+                    suggestion = "; prefer one of " + "".join(
+                        [
+                            "[",
+                            ", ".join(
+                                f"{b.__name__}.{attr_nm}"
+                                for attr_nm in derive_from.keys()
+                            ),
+                            "]",
+                        ]
+                    )
+                raise TypeError(
+                    f"type '{b.__name__}' is not an acceptable base type{suggestion}"
+                )
+
+        if derive_from is None or len(derive_from) == 0:
+            raise ValueError(f"{name} should provide a non-empty list for derive_from")
+
+        for alias_nm, alias_ty in derive_from.items():
+            if alias_nm not in namespace:
+                raise AttributeError(
+                    f"{mcs.__qualname__} derive_from: "
+                    f"{alias_nm} not in namespace of {name}; "
+                    f"namespace={namespace}"
+                )
+            elif alias_ty is not namespace[alias_nm]:
+                raise ValueError(
+                    f"{mcs.__qualname__} derive_from: "
+                    f"expected {name}.{alias_nm} to have value {alias_ty.__name__}, "
+                    f"but it is {namespace[alias_nm].__name__}; namespace={namespace}"
+                )
+
+        return type.__new__(mcs, name, bases, namespace, **kwargs)
+
+    def __init__(
+        mcs: FinalNamespaceMeta,
+        name: str,
+        bases: list[type],
+        namespace: dict[str, Any],
+        *,
+        derive_from: dict[str, type] | None = None,
+        **kwargs: Any,
+    ) -> None:
+        type.__init__(mcs, name, bases, namespace, **kwargs)
