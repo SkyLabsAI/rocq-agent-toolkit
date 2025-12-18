471b9133c4035d014fbd8f80f8d7f2747d59c9e3
Rodolphe Lepigre <rodolphe.lepigre@skylabs-ai.com>
2025-12-18 11:58:25 +0100

    Remove [psi-agents].

--- psi-agents/src/psi_agents/hammer_agent.py
+++
@@ -1,7 +0,0 @@
-
-from rocq_pipeline.agent import OneShotAgent
-
-class HammerAgent(OneShotAgent):
-    """Invoke the SkyLabs AI 'hammer' tactic"""
-    def __init__(self):
-        super().__init__("verify_spec; go; try ego")
