580bb6e04472c545c1181d2e9ffece36f07424cb
Gregory Malecha <gregory@skylabs-ai.com>
2025-12-29 20:24:37 -0500

    WIP.

--- rocq-pipeline/src/rocq_pipeline/search/strategy.py
+++ rocq-pipeline/src/rocq_pipeline/search/strategy.py
@@ -3,11 +3,114 @@ from __future__ import annotations
 import heapq
 from abc import ABC, abstractmethod
 from collections.abc import Callable, Iterable, Iterator, Mapping
+from dataclasses import dataclass
 from typing import Any, TypeVar, override
 
 from .action import Action
 
 T_co = TypeVar("T_co", covariant=True)
+
+
+class Rollout[T_co](ABC):
+    """
+    This type is effectively: Iterator[Rollout.Delay | Rollout.Result]
+    """
+
+    @dataclass(frozen=True)
+    class Delay[U]:
+        logprob: float
+
+    @dataclass(frozen=True)
+    class Result[U]:
+        logprob: float
+        result: Action[U]
+
+    @abstractmethod
+    def next(
+        self, min_logprob: float = -float("inf")
+    ) -> Rollout.Delay | Rollout.Result:
+        """
+        strat.next(min_prob=math.log(0.5))
+        - Rollout.Delay(math.log(0.45)) -- i don't have anything with higher prob than 0.5, ask me again when you want something less 0.45
+        - Rollout.Result(math.log(0.6), act) -- action has probability 0.6
+
+        An implementation is only allowed to return `Delay(Pr)` if `Pr < min_logprob`, otherwise, it must produce a value, or raise `StopIteration`.
+
+        raises `StopIteration` when complete
+        """
+        ...
+
+    def __next__(self) -> tuple[float, Action[T_co]]:
+        result = self.next()
+        assert isinstance(result, Rollout.Result)
+        return (result.logprob, result.result)
+
+
+class EmptyRollout[T_co](Rollout[T_co]):
+    @override
+    def next(self, min_prob: float = 0) -> Rollout.Delay | Rollout.Result:
+        raise StopIteration()
+
+
+def empty_Rollout[T]() -> Rollout[T]:
+    return EmptyRollout()
+
+
+class SingletonRollout[T_co](Rollout[T_co]):
+    def __init__(
+        self,
+        value: Action[T_co],
+        *,
+        logprob: float = 0.0,
+    ) -> None:
+        assert logprob <= 0  # validation logic?
+        self._value = value
+        self._logprob = logprob
+        self._done = False
+
+    @override
+    def next(self, min_logprob: float = 0) -> Rollout.Delay | Rollout.Result[T_co]:
+        if self._done:
+            raise StopIteration()
+        if min_logprob > self._logprob:
+            return Rollout.Delay(self._logprob)
+        else:
+            self._done = True
+            return Rollout.Result(self._logprob, self._value)
+
+
+class IterableRollout[T_co](Rollout[T_co]):
+    def __init__(self, iterable: Iterable[tuple[float, Action[T_co]]]) -> None:
+        self._values = iter(iterable)
+
+    @override
+    def next(self, min_logprob: float = 0) -> Rollout.Delay | Rollout.Result:
+        logprob, act = next(self._values)
+        return Rollout.Result(logprob, act)
+
+
+class IteratorRollout[T_co](Rollout[T_co]):
+    def __init__(self, iterator: Iterator[Rollout.Delay | Rollout.Result]) -> None:
+        self._values = iterator
+        self._last: float | None = None
+
+    def next(
+        self, min_logprob: float = -float("inf")
+    ) -> Rollout.Delay | Rollout.Result:
+        if self._last is not None and min_logprob > self._last:
+            return Rollout.Delay(self._last)
+        result = next(self._values)
+        if isinstance(result, Rollout.Delay):
+            self._last = (
+                result.logprob
+                if self._last is None
+                else min(self._last, result.logprob)
+            )
+            # recursive call, but we consumed one value out of the generator
+            return self.next(min_logprob=min_logprob)
+        assert isinstance(result, Rollout.Result)
+        self._last = result.logprob
+        return result
 
 
 class Strategy[T_co](ABC):
@@ -17,9 +120,6 @@ class Strategy[T_co](ABC):
     very large (even infinite) action spaces such as next tactic
     prediction in theorem proving.
     """
-
-    # TODO: make [Rollout] into a class
-    type Rollout[U] = Iterator[tuple[float, Action[U]]]
 
     # Context information must be read-only and constant in order
     # for searches to work correctly. Clients should use an
@@ -48,22 +148,18 @@ class Strategy[T_co](ABC):
         pass
 
 
-def empty_Rollout() -> Strategy.Rollout:
-    yield from ()
-
-
 class SingletonStrategy[T_co](Strategy[T_co]):
-    def __init__(self, value: Action[T_co], prob: float = 1.0) -> None:
+    def __init__(self, value: Action[T_co], logprob: float = 1.0) -> None:
         self._value = value
-        self._prob = prob
+        self._logprob = logprob
 
     def rollout(
         self,
         state: T_co,
         max_rollout: int | None = None,
         context: Strategy.Context | None = None,
-    ) -> Strategy.Rollout[T_co]:
-        return iter([(self._prob, self._value)])
+    ) -> Rollout[T_co]:
+        return SingletonRollout(self._value, logprob=self._logprob)
 
 
 class IteratorStrategy[T_co](Strategy[T_co]):
@@ -75,8 +171,10 @@ class IteratorStrategy[T_co](Strategy[T_co]):
         state: T_co,
         max_rollout: int | None = None,
         context: Strategy.Context | None = None,
-    ) -> Strategy.Rollout[T_co]:
-        return iter(self._collection)
+    ) -> Rollout[T_co]:
+        return IteratorRollout(
+            (Rollout.Result(logprob, act) for logprob, act in self._collection)
+        )
 
 
 class CompositeStrategy[T_co](Strategy[T_co]):
@@ -85,37 +183,143 @@ class CompositeStrategy[T_co](Strategy[T_co]):
     def __init__(self, children: list[Strategy[T_co]]) -> None:
         self._children: list[Strategy[T_co]] = children
 
+    class Rollout[U](Rollout[U]):
+        class Node[V]:
+            def __init__(
+                self,
+                logprob: float,
+                owner: int,
+                action: Action[V] | None,
+                rest: Rollout[V],
+            ) -> None:
+                self.logprob = logprob
+                self.owner = owner
+                self.action = action
+                self.rest = rest
+
+            def __lt__(self, other: CompositeStrategy.Rollout.Node[V]) -> bool:
+                # Note, since we are using `heapq` (which yields the smallest first)
+                # we make sure that we return the largest first
+                if self.logprob != other.logprob:
+                    return other.logprob < self.logprob
+                if self.action is not None and other.action is None:
+                    return True
+                if self.action is None and other.action is None:
+                    return False
+                return self.owner < other.owner
+
+        def __init__(self, children: list[Rollout[U]]) -> None:
+            self._queue: list[CompositeStrategy.Rollout.Node[U]] = []
+
+            for i, rollout in enumerate(children):
+                # We pass 0.0 as the highest probability to allow `Strategy`s
+                # to only advertise their certainty
+                self._push_next(i, rollout, 0.0)
+
+        def _push_next(self, i: int, rollout: Rollout[U], min_logprob=0.0) -> float:
+            try:
+                result = rollout.next(min_logprob)
+            except StopIteration:
+                return min_logprob
+            heapq.heappush(
+                self._queue,
+                CompositeStrategy.Rollout.Node(
+                    result.logprob,
+                    i,
+                    result.result if isinstance(result, Rollout.Result) else None,
+                    rollout,
+                ),
+            )
+            return result.logprob
+
+        @override
+        def next(
+            self, min_logprob: float = -float("inf")
+        ) -> Rollout.Delay | Rollout.Result:
+            # All entries inside this will have `action = None`
+            # Also, the highest item will be first
+            stashed: list[CompositeStrategy.Rollout.Node[U]] = []
+            candidate: CompositeStrategy.Rollout.Node[U] | None = None
+            while True:
+                try:
+                    result = heapq.heappop(self._queue)
+                except IndexError:
+                    break
+
+                if result.action is None:
+                    if result.logprob < min_logprob:
+                        # This is the highest node and it has a probability less than the query
+                        heapq.heappush(self._queue, result)
+                        break
+                    else:
+                        # There might be a lower probability value that we need to yield, so
+                        # we stash this node and try to find the next node
+                        stashed.append(result)
+                        continue
+                else:
+                    heapq.heappush(self._queue, result)
+                    candidate = result
+                    break
+
+            # TODO: Find the highest action in stashed and candidate (down to `min_logprob`)
+            #
+            highest = candidate.logprob if candidate is not None else min_logprob
+            for n in stashed:
+                try:
+                    xxx = n.rest.next(highest)
+                except StopIteration:
+                    continue
+                if isinstance(xxx, Rollout.Result):
+                    if candidate is None:
+                        candidate = CompositeStrategy.Rollout.Node(
+                            xxx.logprob,
+                            n.owner,
+                            xxx.result,
+                            n.rest,
+                        )
+                        highest = candidate.logprob
+                    elif xxx.logprob > candidate.logprob:
+                        heapq.heappush(self._queue, candidate)
+                        candidate = CompositeStrategy.Rollout.Node(
+                            xxx.logprob,
+                            n.owner,
+                            xxx.result,
+                            n.rest,
+                        )
+                        highest = candidate.logprob
+                    else:
+                        heapq.heappush(
+                            self._queue,
+                            CompositeStrategy.Rollout.Node(
+                                xxx.logprob, n.owner, xxx.result, n.rest
+                            ),
+                        )
+                else:
+                    heapq.heappush(
+                        self._queue,
+                        CompositeStrategy.Rollout.Node(
+                            xxx.logprob, n.owner, None, n.rest
+                        ),
+                    )
+            if candidate:
+                assert candidate.action is not None
+                return Rollout.Result(candidate.logprob, candidate.action)
+            else:
+                return Rollout.Delay(highest)
+
     @override
     def rollout(
         self,
         state: T_co,
         max_rollout: int | None = None,
         context: Strategy.Context | None = None,
-    ) -> Strategy.Rollout[T_co]:
-        def combine() -> Strategy.Rollout[T_co]:
-            queue: list[tuple[float, int, Action[T_co], Strategy.Rollout]] = []
-
-            def push_next(i: int, g: Strategy.Rollout[T_co]) -> None:
-                nonlocal queue
-                try:
-                    pr, act = next(g)
-                except StopIteration:
-                    return
-                heapq.heappush(queue, (-pr, i, act, g))
-
-            for i, strat in enumerate(self._children):
-                gen = strat.rollout(state, max_rollout=max_rollout, context=context)
-                push_next(i, gen)
-
-            while True:
-                try:
-                    (pr, i, act, gen) = heapq.heappop(queue)
-                except IndexError:
-                    return
-                yield (-pr, act)
-                push_next(i, gen)
-
-        return combine()
+    ) -> Rollout[T_co]:
+        return CompositeStrategy.Rollout(
+            [
+                strat.rollout(state, max_rollout, context=context)
+                for strat in self._children
+            ]
+        )
 
 
 class FailStrategy[T_co](Strategy[T_co]):
@@ -127,7 +331,7 @@ class FailStrategy[T_co](Strategy[T_co]):
         state: T_co,
         max_rollout: int | None = None,
         context: Strategy.Context | None = None,
-    ) -> Strategy.Rollout:
+    ) -> Rollout[T_co]:
         return empty_Rollout()
 
 
@@ -149,7 +353,7 @@ class GuardStrategy[T_co, With](FailStrategy[T_co], ABC):
         rdm: T_co,
         max_rollout: int | None = None,
         context: Strategy.Context | None = None,
-    ) -> Strategy.Rollout: ...
+    ) -> Rollout[T_co]: ...
 
     @override
     def rollout(
@@ -157,7 +361,7 @@ class GuardStrategy[T_co, With](FailStrategy[T_co], ABC):
         state: T_co,
         max_rollout: int | None = None,
         context: Strategy.Context | None = None,
-    ) -> Strategy.Rollout:
+    ) -> Rollout[T_co]:
         val = self.check(state)
         if val is None:
             return super().rollout(state, max_rollout, context)
@@ -174,28 +378,30 @@ class ActionWrapper[T_co](Action[T_co]):
         return self._base.interact(state)
 
 
-class TraceStrategy[T_co](Strategy[T_co]):
-    def __init__(self, base: Strategy[T_co]) -> None:
-        self._base = base
-        self._trace: list[tuple[T_co, Action[T_co]]] = []
+# class TraceStrategy[T_co](Strategy[T_co]):
+#     def __init__(self, base: Strategy[T_co]) -> None:
+#         self._base = base
+#         self._trace: list[tuple[T_co, Action[T_co]]] = []
 
-    @property
-    def trace(self) -> list[tuple[T_co, Action[T_co]]]:
-        return self._trace
+#     @property
+#     def trace(self) -> list[tuple[T_co, Action[T_co]]]:
+#         return self._trace
 
-    @override
-    def rollout(
-        self,
-        state: T_co,
-        max_rollout: int | None = None,
-        context: Strategy.Context | None = None,
-    ) -> Strategy.Rollout[T_co]:
-        roll = self._base.rollout(state, max_rollout, context)
+#     @override
+#     def rollout(
+#         self,
+#         state: T_co,
+#         max_rollout: int | None = None,
+#         context: Strategy.Context | None = None,
+#     ) -> Rollout[T_co]:
+#         roll = self._base.rollout(state, max_rollout, context)
 
-        def mk(action: Action[T_co]) -> Callable[[T_co], None]:
-            def fn(state: T_co) -> None:
-                self._trace.append((state, action))
+#         def mk(action: Action[T_co]) -> Callable[[T_co], None]:
+#             def fn(state: T_co) -> None:
+#                 self._trace.append((state, action))
 
-            return fn
+#             return fn
 
-        return ((prob, ActionWrapper(action, mk(action))) for prob, action in roll)
+#         return IteratorRollout(
+#             ((prob, ActionWrapper(action, mk(action))) for prob, action in roll)
+#         )
