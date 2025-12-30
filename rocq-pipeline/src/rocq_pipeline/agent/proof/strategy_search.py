ba0fc6f8e2ad29500eecd7938d4a088cb3142c7b
Gregory Malecha <gregory@skylabs-ai.com>
2025-12-30 09:02:08 -0500

    Start hacking.

--- rocq-pipeline/src/rocq_pipeline/agent/proof/strategy_search.py
+++ rocq-pipeline/src/rocq_pipeline/agent/proof/strategy_search.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 import heapq
 import math
 from dataclasses import dataclass
-from typing import override
+from typing import Any, override
 
 from rocq_doc_manager import RocqCursor
 
@@ -9,6 +11,15 @@ from rocq_pipeline.agent.base import TaskResult
 from rocq_pipeline.agent.base.classes import ProofAgent
 from rocq_pipeline.proof_state import ProofState
 from rocq_pipeline.search import Action
+from rocq_pipeline.search.rocq.manip import RocqManipulator
+from rocq_pipeline.search.search import (
+    DFS,
+    Frontier,
+    PQueue,
+    SavingSolutions,
+    StateManipulator,
+)
+from rocq_pipeline.search.search import search as search_strategy
 from rocq_pipeline.search.strategy import Strategy
 
 
@@ -17,7 +28,7 @@ class SearchAgent(ProofAgent, VERSION="0.1.0"):
     A simple search agent based on strategies.
     """
 
-    def __init__(self, strategy: Strategy, fuel: int | None = 100) -> None:
+    def __init__(self, strategy: Strategy[RocqCursor], fuel: int | None = 100) -> None:
         self._strategy = strategy
         self._fuel = fuel
 
@@ -25,13 +36,44 @@ class SearchAgent(ProofAgent, VERSION="0.1.0"):
     class State:
         log_prob: float  # negative
         depth: int  # negative
-        fresh: int  # just to break ties
         cursor: RocqCursor
         rollout: Strategy.Rollout
 
+    class Manipulator(StateManipulator[State]):
+        def __init__(self) -> None:
+            self._rocq = RocqManipulator()
+
+        def copy(self, state: SearchAgent.State) -> SearchAgent.State:
+            return SearchAgent.State(
+                state.log_prob,
+                state.depth,
+                self._rocq.copy(state.cursor),
+                state.rollout,
+            )
+
+        def dispose(self, state: SearchAgent.State) -> None:
+            self._rocq.dispose(state.cursor)
+
     @override
     def prove(self, rc: RocqCursor) -> TaskResult:
-        next_id: int = 0
+        def mk_frontier() -> Frontier[SearchAgent.State, Any]:
+            def cmp(st1: SearchAgent.State, st2: SearchAgent.State) -> int:
+                if st1.depth != st2.depth:
+                    # Prefer deeper nodes
+                    return st1.depth - st2.depth
+                if st1.log_prob < st2.log_prob:
+                    return -1
+                if st1.log_prob > st2.log_prob:
+                    return 1
+                return 0
+
+            base = PQueue(lambda x: x, cmp)
+            return SavingSolutions(base, lambda st: True, stop_on_first_solution=True)
+
+        init = SearchAgent.State(-math.log(1.0), 0, rc, self._strategy.rollout(rc))
+        search_strategy(
+            self._strategy, init, mk_frontier, state_manip=RocqManipulator()
+        )
 
         def fresh() -> int:
             nonlocal next_id
