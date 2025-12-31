8b617ea97fd30de0025920644d22a79f8dcdf9af
Gregory Malecha <gregory@skylabs-ai.com>
2025-12-30 21:17:21 -0500

    Cleanup the wrapper based on review feedback.

--- rocq-pipeline/src/rocq_pipeline/agent/proof/trace_cursor.py
+++ rocq-pipeline/src/rocq_pipeline/agent/proof/trace_cursor.py
@@ -16,40 +16,30 @@ def _trace_log(
     *,
     after: bool = False,
     cmd: Callable[[dict[str, Any]], str] | None = None,
-    inputs_ex: Callable[[RocqCursor, dict[str, Any]], Any] | None = None,
-    inputs: Callable[[dict[str, Any]], dict[str, Any]] | None = None,
+    inputs: Callable[[RocqCursor, dict[str, Any]], Any] | None = None,
     output: Callable[[Any], Any] | None = None,
     exception: Callable[[Any], Any] | None = None,
 ):
+    fn_input = (lambda _, args: args) if inputs is None else inputs
+    fn_output = (lambda x: x) if output is None else output
+    fn_except = (lambda x: x) if exception is None else exception
+
     def wrap(func: Callable):
         @functools.wraps(func)
-        def wrapper(self, **args):
+        def wrapper(self, **kwargs):
             # it is important that we get the location before we run the function
-            location = self._location()
-            log_args = {"state_id": location}
-            if inputs_ex:
-                assert inputs is None
-                log_args["args"] = inputs_ex(self, args)
-            elif inputs:
-                log_args["args"] = inputs(args)
-            else:
-                log_args["args"] = args
+            log_args = {"state_id": self._location()}
+            log_args["args"] = fn_input(self, kwargs)
             if cmd:
-                log_args["action"] = cmd(args)
+                log_args["action"] = cmd(kwargs)
             try:
-                result = func(self, **args)
-                if output:
-                    log_args["result"] = output(result)
-                else:
-                    log_args["result"] = result
+                result = func(self, **kwargs)
+                log_args["result"] = fn_output(result)
                 if after:
                     log_args["next_state_id"] = self._location()
                 logger.info(f"RocqCursor.{func.__name__}", **log_args)
             except Exception as err:
-                if exception:
-                    log_args["exception"] = exception(err)
-                else:
-                    log_args["exception"] = err
+                log_args["exception"] = fn_except(err)
                 logger.info(f"RocqCursor.{func.__name__}", **log_args)
                 raise
 
@@ -87,14 +77,14 @@ class TracingCursor(RocqCursor):
         return TracingCursor(result._the_rdm, result._cursor)
 
     @override
-    @_trace_log(after=True, inputs=lambda args: args["text"])
+    @_trace_log(after=True, inputs=lambda _, args: args["text"])
     def insert_command(
         self, text: str, blanks: str | None = "\n", safe: bool = True
     ) -> RocqCursor.CommandData | RocqCursor.Err[RocqCursor.CommandError]:
         return super().insert_command(text, blanks, safe)
 
     @override
-    @_trace_log(after=True, inputs=lambda args: args["text"])
+    @_trace_log(after=True, inputs=lambda _, args: args["text"])
     def run_command(self, text: str) -> RocqCursor.CommandData | RocqCursor.Err[None]:
         return super().run_command(text)
 
@@ -104,38 +94,38 @@ class TracingCursor(RocqCursor):
         return suffix[0]
 
     @override
-    @_trace_log(after=True, inputs_ex=_next_command)
+    @_trace_log(after=True, inputs=_next_command)
     def run_step(
         self,
     ) -> RocqCursor.CommandData | None | RocqCursor.Err[RocqCursor.CommandError | None]:
         return super().run_step()
 
     @override
-    @_trace_log(after=False, inputs=lambda args: args["text"])
+    @_trace_log(inputs=lambda _, args: args["text"])
     def query(self, text: str) -> RocqCursor.CommandData | RocqCursor.Err[None]:
         return super().query(text)
 
     @override
-    @_trace_log(after=False, inputs=lambda args: args["text"])
+    @_trace_log(inputs=lambda _, args: args["text"])
     def query_json(
         self, text: str, index: int
     ) -> Any | RocqCursor.Err[RocqCursor.CommandError]:
         return super().query_json(text, index)
 
     @override
-    @_trace_log(after=False, inputs=lambda args: args["text"])
+    @_trace_log(inputs=lambda _, args: args["text"])
     def query_json_all(
         self, text: str, indices: list[int] | None = None
     ) -> list[Any] | RocqCursor.Err[None]:
         return super().query_json_all(text, indices)
 
     @override
-    @_trace_log(after=False, inputs=lambda args: args["text"])
+    @_trace_log(inputs=lambda _, args: args["text"])
     def query_text(self, text: str, index: int) -> str | RocqCursor.Err[None]:
         return super().query_text(text, index)
 
     @override
-    @_trace_log(after=False, inputs=lambda args: args["text"])
+    @_trace_log(inputs=lambda _, args: args["text"])
     def query_text_all(
         self, text: str, indices: list[int] | None = None
     ) -> list[str] | RocqCursor.Err[None]:
