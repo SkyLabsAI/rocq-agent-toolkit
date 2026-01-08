76f6c698b463b7077a04b18c08ce36b55d4cd493
Gregory Malecha <gregory@skylabs-ai.com>
2026-01-08 14:52:48 -0500

    A simple oracle agent (this breaks some abstractions)

---
+++ rocq-pipeline/tests/test_oracle_agent.py
@@ -0,0 +1,21 @@
+import tempfile
+
+import rocq_pipeline.task_runner
+from rocq_pipeline.agent import AgentBuilder
+from rocq_pipeline.agent.proof.oracle_agent import OracleAgent
+
+from .util import make_task_str
+
+
+def test_oracle_agent() -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        result = rocq_pipeline.task_runner.agent_main(
+            AgentBuilder.of_agent(OracleAgent),
+            [
+                "--task-json",
+                make_task_str("examples/theories/test_simple.v", "Lemma:is_true"),
+                "--output-dir",
+                temp_dir,
+            ],
+        )
+    assert result
