e0196a242f53354b9a613722d1140b0f535010e1
Gregory Malecha <gregory@skylabs-ai.com>
2026-01-08 23:17:43 -0500

    Remove internal dependencies.

--- rocq-pipeline/tests/test_find_task.py
+++ rocq-pipeline/tests/test_find_task.py
@@ -18,28 +18,3 @@ def test_with_deps() -> None:
     tasks = rocq_pipeline.find_tasks.find_tasks(Path("examples/theories/with_dep.v"))
     assert len(tasks) == 1
     assert tasks == [{"locator": "Lemma:add_comm", "tags": ["proof"]}]
-
-
-def test_factorial_longlong() -> None:
-    tasks = rocq_pipeline.find_tasks.find_tasks(
-        Path("tests/Factorial_longlong.v"), tagger=rocq_pipeline.find_tasks.my_tagger
-    )
-    assert len(tasks) == 2
-    test_ok_inv_task = tasks[1]
-    assert test_ok_inv_task["locator"] == "Lemma:test_ok_inv"
-    tags = test_ok_inv_task["tags"]
-    assert set(tags) == {
-        "proof",
-        "qed",
-        "Arith.arith_simpl",
-        "rewrite",
-        "intros",
-        "wp_for-loopinv",
-        "assert",
-        "go",
-        "lia",
-        "verify_spec",
-        "specialize",
-        "NumTactics=18",
-        "UnmatchedTactics=['+', '{', '}']",
-    }
