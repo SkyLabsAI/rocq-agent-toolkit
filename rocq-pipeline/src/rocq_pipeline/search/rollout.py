97b9c4f90067d279eddb35269f8e527dfabc4a84
Gregory Malecha <gregory@skylabs-ai.com>
2026-01-25 04:48:45 -0500

    A de-duplicating Rollout.

--- rocq-pipeline/src/rocq_pipeline/search/rollout.py
+++ rocq-pipeline/src/rocq_pipeline/search/rollout.py
@@ -426,3 +426,32 @@ class Delay[Action](Rollout[Action]):
             self._base = self._base()
             return self._base.next(min_logprob=min_logprob)
         return Rollout.Approx(logprob=self._wait, result=None)
+
+
+class DeduplicateRollout[T_co](Rollout[T_co]):
+    def __init__(
+        self,
+        rollout: Rollout[T_co],
+        *,
+        compare: Callable[[T_co, T_co], bool] = lambda x, y: x is y,
+    ) -> None:
+        super().__init__()
+        self._base = rollout
+        self._seen: list[T_co] = []
+        self._compare = compare
+
+    def _add(self, candidate: T_co) -> bool:
+        if any(self._compare(x, candidate) for x in self._seen):
+            return False
+        self._seen.append(candidate)
+        return True
+
+    @override
+    def next(self, min_logprob: float = NEG_INF) -> Rollout.Approx[T_co]:
+        while True:
+            proposal = self._base.next(min_logprob=min_logprob)
+            if proposal.result is None:
+                return proposal
+            if self._add(proposal.result):
+                return proposal
+            # we already returned this proposal, so we try to get the next one
---
+++ rocq-pipeline/tests/rocq_pipeline/search/test_deduplicate_rollout.py
@@ -0,0 +1,49 @@
+from rocq_pipeline.search.rollout import (
+    DeduplicateRollout,
+    IterableRollout,
+    Rollout,
+    empty_Rollout,
+    singleton,
+)
+
+from .rollout_util import is_empty
+
+
+def test_empty() -> None:
+    rollout: Rollout[int] = DeduplicateRollout(empty_Rollout())
+
+    is_empty(rollout)
+
+
+def test_singleton() -> None:
+    rollout: Rollout[int] = DeduplicateRollout(singleton(1, score=-1.0))
+
+    assert list(rollout) == [(-1, 1)]
+    is_empty(rollout)
+
+
+def test_multiple() -> None:
+    rollout: Rollout[int] = DeduplicateRollout(
+        IterableRollout(iter([(1, 1), (0.5, 2), (0.25, 3)]))
+    )
+
+    assert list(rollout) == [(1, 1), (0.5, 2), (0.25, 3)]
+    is_empty(rollout)
+
+
+def test_duplicate() -> None:
+    rollout: Rollout[int] = DeduplicateRollout(
+        IterableRollout(iter([(1, 1), (0.5, 1), (0.25, 3)]))
+    )
+
+    assert list(rollout) == [(1, 1), (0.25, 3)]
+    is_empty(rollout)
+
+
+def test_duplicate2() -> None:
+    rollout: Rollout[int] = DeduplicateRollout(
+        IterableRollout(iter([(1, 1), (0.75, 1), (0.5, 1), (0.25, 3)]))
+    )
+
+    assert list(rollout) == [(1, 1), (0.25, 3)]
+    is_empty(rollout)
