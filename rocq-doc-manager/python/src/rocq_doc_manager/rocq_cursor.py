583e7128a766b34a91524b882c8e3cc6652630a3
Jan-Oliver Kaiser <janno@skylabs-ai.com>
2026-01-30 11:55:37 +0100

    Remove impossible branch

--- rocq-doc-manager/python/src/rocq_doc_manager/rocq_cursor.py
+++ rocq-doc-manager/python/src/rocq_doc_manager/rocq_cursor.py
@@ -124,11 +124,7 @@ class RocqCursor(RocqCursorProtocol):
         self, text: str, blanks: str | None = "\n", safe: bool = True
     ) -> RocqCursor.CommandData | RocqCursor.Err[RocqCursor.CommandError]:
         if safe:
-            prefix_reply = self.doc_prefix()
-            if isinstance(prefix_reply, API.Err):
-                # This is okay because the error is a cursor error
-                return prefix_reply
-            prefix: list[API.PrefixItem] = prefix_reply
+            prefix = self.doc_prefix()
             if prefix != [] and prefix[-1].kind != "blanks":
                 self.insert_blanks(" ")
                 revert = True
