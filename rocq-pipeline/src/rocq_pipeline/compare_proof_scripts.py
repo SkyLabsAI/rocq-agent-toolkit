41aee086c4b090456967e0327d9ceb5ffd7b8f2d
Lennart Beringer <lennart.beringer@skylabs-ai.com>
2026-02-02 13:02:51 -0500

    Added variant that accesses groundtruth without using RDM

--- rocq-pipeline/src/rocq_pipeline/compare_proof_scripts.py
+++ rocq-pipeline/src/rocq_pipeline/compare_proof_scripts.py
@@ -1,11 +1,14 @@
 import json
 import logging
+import os
+from pydoc import text
 import sys
 from pathlib import Path
 from typing import Any, Literal
 
 from rocq_doc_manager import DuneUtil, RocqCursor, RocqDocManager
 
+from rocq_pipeline.find_tasks import scan_proof
 from rocq_pipeline.locator import FirstLemma
 
 # from rocq_pipeline.rocq_cursor import RocqCursor
@@ -52,7 +55,7 @@ def extract_nth_inferred_proofscript(
     try:
         with open(file_path, encoding="utf-8") as f:
             line_count = sum(1 for _ in f)
-            print(f"File: {file_path} has {line_count} lines.")
+            # print(f"File: {file_path} has {line_count} lines.")
             f.seek(0)  # Reset file pointer to beginning
             for i, line in enumerate(f):
                 if i == target_index:
@@ -63,114 +66,132 @@ def extract_nth_inferred_proofscript(
                         task_id = data.get("task_id", "Unknown")
                         status = data.get("status", "Unknown")
                         inferred_proof_script: list[str] | None = get_proofscript(data)
-                        print(
-                            f"Line {i + 1}: Status = {str(status)}, Task_id = {str(task_id)}"
-                        )
+                        # print(f"Line {i + 1}: Status = {str(status)}, Task_id = {str(task_id)}")
+
                         if inferred_proof_script is None:
-                            print(f"❌ Error: proof script found in line {n} is None.")
+                            logger.error(f"❌ Error: proof script found in line {n} is None.")
                             return None
                         if isinstance(task_id, str) and isinstance(status, str):
                             status_literal: Literal["Success", "Failure"]
                             status_literal = (
                                 "Success" if status.lower() == "success" else "Failure"
                             )
-                            # print(
-                            #    f"Inferred script: {str(inferred_proof_script)}, status = {status_literal}"
-                            # )
                             return (task_id, inferred_proof_script, status_literal)
                         else:
                             return None
                     except json.JSONDecodeError:
-                        # print(f"❌ Error: Line {n} is not valid JSON.")
+                        logger.error(f"❌ Error: Line {n} is not valid JSON.")
                         return None
 
         # If the loop finishes without returning, N is out of bounds
-        print(f"❌ Error: File {file_path} contains fewer than {n} lines.")
+        logger.error(f"❌ Error: File {file_path} contains fewer than {n} lines.")
         return None
 
     except FileNotFoundError:
-        print(f"❌ Error: The file '{file_path}' was not found.")
+        logger.error(f"❌ Error: The file '{file_path}' was not found.")
         return None
     except Exception as e:
-        print(f"❌ An unexpected error occurred: {e}")
+        logger.error(f"❌ An unexpected error occurred: {e}")
         return None
 
+def find_lemma (lemma:str):
+    def inner(text: str, kind: Literal["blanks", "command", "ghost"]) -> bool:
+        parts = text.split(None, 1)
+        if len(parts) != 2:
+            return False
+        thm_or_lemma = parts[0]
+        # The split handles leading space on x and space between x and y,
+        # but y might still have trailing whitespace.
+        statement = parts[1].strip()
+        if thm_or_lemma in ["Theorem", "Lemma"]:
+            lemmaname = statement.split(":", 1)[0] if ":" in statement else statement
+            searchstring = (f"{thm_or_lemma}:{lemmaname}").strip()
+            res: bool = kind == "command" and searchstring == lemma.strip()
+        else:
+            res = False
+        return res
+    return inner
 
 def get_groundtruth_script(
-    task_id: str, rocqfiles_path: str
-) -> tuple[list[str], Literal["Success", "Failure"]] | None:
+        task_id: str, rocqfiles_path: str
+        ) -> tuple[FirstLemma, Path] | None:
     if task_id.count("#") != 1:
         raise ValueError("task_id must contain exactly one # separating two parts.")
 
-    rocq_file: str
-    my_locator: str
     rocq_file, my_locator = task_id.split("#", 1)
+
+    first_lemma: FirstLemma | None = FirstLemma.parse(my_locator)
+    if first_lemma is None:
+        return None
+
     gt_file = Path(rocqfiles_path) / rocq_file
-    # print(
-    #     f"Getting groundtruth for path {rocqfiles_path}, file: {rocq_file}, Locator: {my_locator}"
-    # )
+    if not os.path.exists(gt_file):
+        logger.error(f"❌ Error: groundtruth file not found: {gt_file}")
+        return None
+    return first_lemma, gt_file
+
+def get_groundtruth_script_via_RDM(
+    task_id: str, rocqfiles_path: str
+) -> tuple[list[str], Literal["Success", "Failure"]] | None:
+    gt = get_groundtruth_script(task_id, rocqfiles_path)
+    if gt is None:
+        return None
+    first_lemma, gt_file = gt
 
     args: list[str] = DuneUtil.rocq_args_for(gt_file)
-    # print(f'args: {str(args)}')
     with RocqDocManager(args, str(gt_file), dune=True).sess(load_file=True) as rdm:
         rc: RocqCursor = rdm.cursor()
 
-        first_lemma: FirstLemma | None = FirstLemma.parse(str(my_locator))
-        if first_lemma is None:
-            return None
-
-        def find_lemma(text: str, kind: Literal["blanks", "command", "ghost"]) -> bool:
-            parts = text.split(None, 1)  # splitting on whitespace
-            if len(parts) != 2:
-                return False  # raise ValueError("Input string must contain two parts separated by whitespace.")
-            thm_or_lemma = parts[0]
-            # The split handles leading space on x and space between x and y,
-            # but y might still have trailing whitespace.
-            statement = parts[1].strip()
-            if thm_or_lemma in ["Theorem", "Lemma"]:
-                lemmaname = (
-                    statement.split(":", 1)[0] if ":" in statement else statement
-                )
-                searchstring = (f"{thm_or_lemma}:{lemmaname}").strip()
-                # print(f"Comparing lemma: kind={str(kind)}, searchstring={searchstring} with first_lemma={str(first_lemma)}")
-                res: bool = (
-                    kind == "command" and searchstring == (str(first_lemma)).strip()
-                )
-            else:
-                res = False
-            return res
-
-        found = rc.goto_first_match(find_lemma)
+        found = rc.goto_first_match(find_lemma(str(first_lemma)))
         if not found:
-            print(f"❌ Error: Could not find lemma {str(first_lemma)} in file.")
-            return None
-        index: int = rc.cursor_index()
-        script_start: None | RocqCursor.Err[RocqCursor.CommandError | None] = rc.go_to(
-            index + 3
-        )  # step to beginning of proof script
-        if script_start is not None:
-            print("❌ Error: Could not navigate to proof script start.")
+            logger.error(f"❌ Error: Could not find lemma {str(first_lemma)} in file.")
             return None
         suffix: list[RocqCursor.SuffixItem] = rc.doc_suffix()
-        # num_suffixes = len(suffix)
-        # print(f"Number of suffix items after locating lemma: {num_suffixes}")
-        gt_script: list[str] = []
-        status: Literal["Success", "Failure"] = "Failure"
-        i = 0
-        while i < len(suffix):
-            item = suffix[i]
-            txt = item.text
-            if not txt.isspace():
-                gt_script.append(txt)
-                if txt == "Qed.":
-                    status = "Success"
-                if txt in ["Qed.", "Admitted.", "Abort."]:
-                    break
-            i += 1
-        # print(f"Groundtruth script: {str(gt_script)}")
+        if len(suffix) == 0:
+            logger.error("❌ Error: Suffix after locating lemma is empty.")
+            return None
+        prooftask = scan_proof(suffix[1:])  # skip the first item (the lemma statement itself)
+
+        gt_script = prooftask.proof_tactics
+        status: Literal["Success", "Failure"] = "Success" if prooftask.final == "qed" else "Failure"
 
         return (gt_script, status)
 
+def get_groundtruth_script_from_vfile(
+    task_id: str, rocqfiles_path: str
+) -> tuple[list[str], Literal["Success", "Failure"]] | None:
+    gt = get_groundtruth_script(task_id, rocqfiles_path)
+    if gt is None:
+        return None
+    first_lemma, gt_file = gt
+
+    with open(gt_file, encoding="utf-8") as f:
+        lines = f.readlines()
+    found = False
+    index = i + 1
+    for i, line in enumerate(lines):
+        if find_lemma(str(first_lemma))(line, "command"):
+            found = True
+            index = i + 1
+            break
+    if not found:
+        logger.error(f"❌ Error: Could not find locator {str(first_lemma)} in file.")
+        return None
+
+    script: list[str] = []
+    while index < len(lines):
+        line = f"{lines[index].strip()} " #Add trailing space here so we can split on '. ' below
+        if line.startswith("Qed.") or line.startswith("Admitted.") or line.startswith("Abort."):
+            status: Literal["Success", "Failure"] = (
+                "Success" if line.startswith("Qed.") else "Failure"
+            )
+            return (script, status)
+        if not line.startswith("Proof") and not line.startswith("cpp_sound"):
+            segments = line.split('. ')
+            cleaned_segments = [f'{segment.strip()}.' for segment in segments if segment.strip()]
+            script += cleaned_segments
+        index += 1
+    return None
 
 def extract_statistics(
     input_path: str, line_num: int, rocqfiles_path: str
@@ -183,15 +204,22 @@ def extract_statistics(
     task_id, inf_script, inf_status = inferred
     i_script = " ".join(inf_script)
 
-    groundtruth: tuple[list[str], Literal["Success", "Failure"]] | None
-    groundtruth = get_groundtruth_script(task_id, rocqfiles_path)
+    gt_v = get_groundtruth_script_from_vfile(task_id, rocqfiles_path)
+    if gt_v is None:
+        return None
+    v_status: Literal["Success", "Failure"]
+    v_script, v_status = gt_v
+
+    groundtruth = get_groundtruth_script_via_RDM(task_id, rocqfiles_path)
     if groundtruth is None:
         return None
-    gt_script: list[str]
     gt_status: Literal["Success", "Failure"]
     gt_script, gt_status = groundtruth
     g_script = " ".join(gt_script)
     res: dict[str, Any] = {
+        "v_length": len(v_script),
+        "v_script": " ".join(v_script),
+        "v_proof_status": v_status,
         "groundtruth_length": len(gt_script),
         "groundtruth_script": g_script,
         "groundtruth_proof_status": gt_status,
