dbed930d7f6d57f29b05310f56391642a9fdd706
Rodolphe Lepigre <rodolphe.lepigre@skylabs-ai.com>
2026-02-03 16:14:17 +0100

    [jsonrpc-tp] add low-level async python interface.
    Co-Authored-By: Jan-Oliver Kaiser <janno@skylabs-ai.com>

---
+++ ocaml-jsonrpc-tp/python/tests/test_async.py
@@ -0,0 +1,51 @@
+import asyncio
+
+from jsonrpc_tp import AsyncJsonRPCTP as API
+from jsonrpc_tp import Resp
+
+
+def test_trivial_shutdown():
+    async def main():
+        api = API(["dune", "exec", "jsonrpc-tp.delayed-echo-api", "--", "4"])
+        await api.start()
+        await api.quit()
+
+    asyncio.run(main())
+
+
+def test_echo():
+    async def main():
+        n = 0
+
+        api_ref = None
+
+        async def handle_notification(method: str, params: dict) -> None:
+            nonlocal n
+            n += 1
+            await api_ref.raw_notification("ok", [])
+
+        api = API(
+            ["dune", "exec", "jsonrpc-tp.delayed-echo-api", "--", "4"],
+            handle_notification=handle_notification,
+        )
+        api_ref = api
+        await api.start()
+
+        messages = ["Bye!", "Coucou!", "Hello!", "Bye!"]
+
+        responses = await asyncio.gather(
+            api.raw_request("echo", [2, messages[0]]),
+            api.raw_request("echo", [3, messages[1]]),
+            api.raw_request("echo", [1, messages[2]]),
+            api.raw_request("echo", [0, messages[3]]),
+        )
+
+        for i in range(4):
+            r = responses[i]
+            assert isinstance(r, Resp)
+            assert r.result == messages[i]
+
+        await api.quit()
+        assert n == 4
+
+    asyncio.run(main())
