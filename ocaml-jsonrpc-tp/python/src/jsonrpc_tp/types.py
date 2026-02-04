9c9849fa992e1847b3de670fcce26f3de7c9cfd6
Jasper Haag <jasper@skylabs-ai.com>
2026-02-04 14:11:38 -0500

    fix(ocaml-jsonrpc-tp/python): tweak Err/Resp types so mypy correctly captures covariance

--- ocaml-jsonrpc-tp/python/src/jsonrpc_tp/types.py
+++ ocaml-jsonrpc-tp/python/src/jsonrpc_tp/types.py
@@ -1,25 +1,115 @@
-from dataclasses import dataclass
+from typing import Any, TypeVar, override
+from warnings import deprecated
 
-# Note: dataclass doesn't play nicely with covariant data, cf.
-# https://discuss.python.org/t/make-replace-stop-interfering-with-variance-inference/96092/41
-
-
-# We use a workaround from the thread above that simply sets `__replace__ = None`
-@dataclass
-class Err[T]:
-    """JSON-RPC error response, with a message and payload."""
-
-    __replace__ = None
-    message: str
-    data: T
+# Note: this file uses custom implementations of Reply/Err/Resp because dataclass
+# doesn't play nicely with covariant data, cf. github.com/python/mypy/issues/17623
+#
+# TODO: use a simpler dataclass based solution that sets `__replace__ = None` when we
+# fully move to pyright, cf. https://github.com/microsoft/pyright/discussions/11012;
+# when we move to pydantic instead of dataclass we may still need to use an explicit
+# covariant `TypeVar` here.
+T_co = TypeVar("T_co", covariant=True)
 
 
-@dataclass
-class Resp[R]:
-    """JSON-RPC response, with a payload."""
+class Reply[T_co]:
+    """Base type of JsonRPC replies."""
 
-    __replace__ = None
-    result: R
+    def __init__(self, data: T_co) -> None:
+        self._data = data
+
+    def __bool__(self) -> bool:
+        raise NotImplementedError(
+            f"implement in derivers of {self.__class__.__qualname__}"
+        )
+
+    def __eq__(self, other: Any) -> Any:
+        if not issubclass(type(other), Reply):
+            return NotImplemented
+        return (
+            type(self) is type(other)
+            and type(self._data) is type(other._data)
+            and self._data == other._data
+        )
+
+    @override
+    def __repr__(self) -> str:
+        arg_reprs = ", ".join(
+            [
+                f"data={repr(self.data)}",
+            ]
+        )
+        return f"{self.__class__.__qualname__}({arg_reprs})"
+
+    @property
+    def data(self) -> T_co:
+        return self._data
+
+
+class Err[T_co](Reply[T_co]):
+    """JSON-RPC error response, with a message and optional payload."""
+
+    def __init__(self, message: str, data: T_co) -> None:
+        super().__init__(data=data)
+        self._message = message
+
+    @override
+    def __repr__(self) -> str:
+        arg_reprs = ", ".join(
+            [
+                f"message={repr(self.message)}",
+                f"data={repr(self.data)}",
+            ]
+        )
+        return f"{self.__class__.__qualname__}({arg_reprs})"
+
+    @override
+    def __bool__(self) -> bool:
+        return False
+
+    @override
+    def __eq__(self, other: Any) -> Any:
+        super_eq = super().__eq__(other)
+        if super_eq is NotImplemented:
+            return NotImplemented
+        return super_eq and self.message == other.message
+
+    @property
+    def message(self) -> str:
+        return self._message
+
+    # Note: we inherit data property from Reply
+
+
+class Resp[T_co](Reply[T_co]):
+    """Json-RPC response, with a payload."""
+
+    def __init__(self, result: T_co) -> None:
+        super().__init__(data=result)
+
+    @override
+    def __repr__(self) -> str:
+        arg_reprs = ", ".join(
+            [
+                f"result={repr(self.result)}",
+            ]
+        )
+        return f"{self.__class__.__qualname__}({arg_reprs})"
+
+    @override
+    def __bool__(self) -> bool:
+        return True
+
+    # Note: while we inherit the data property from Reply, we
+    # prefer to use the name result.
+    @override
+    @property
+    @deprecated("Prefer Resp.result")
+    def data(self) -> T_co:
+        return super().data
+
+    @property
+    def result(self) -> T_co:
+        return super().data
 
 
 class Error(Exception):
