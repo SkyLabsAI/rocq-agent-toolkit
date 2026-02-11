2eebca5c349065f4707f6b7531900a69ece44aa7
Ehtesham Zahoor <ehtesham.zahoor@skylabs-ai.com>
2026-02-11 08:26:32 +0500

    improve formatting and fix a missing import

--- remote_agent_client/src/remote_agent_client/cli.py
+++ remote_agent_client/src/remote_agent_client/cli.py
@@ -2,6 +2,8 @@
 import rocq_pipeline.task_runner as RAT
 
 from remote_agent_client.builder import RemoteProofAgentBuilder
+
+
 def main() -> bool:
     """Main entrypoint for remote-proof-agent."""
     return RAT.agent_main(RemoteProofAgentBuilder())
