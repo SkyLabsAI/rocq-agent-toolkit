06d6fc417c5627eabd2ea1b3313502036fe9b6ce
Jan-Oliver Kaiser <janno@skylabs-ai.com>
2026-02-18 13:49:02 +0100

    Restore test_util.py (and remove now useless test_constants)

--- rocq-doc-manager/python/tests/test_util.py
+++ rocq-doc-manager/python/tests/test_util.py
@@ -5,29 +5,24 @@ from rocq_doc_manager import rocq_doc_manager_api as rdm_api
 from .util import RDM_Tests
 
 
-# class Test_RDM_Tests(RDM_Tests):
-#     @pytest.mark.asyncio
-#     @staticmethod
-#     async def test_fixtures_ok(
-#         transient_shared_rdm: AsyncRocqDocManager,
-#         transient_rdm: AsyncRocqDocManager,
-#         loadable_rdm: AsyncRocqDocManager,
-#     ) -> None:
-#         assert not isinstance(
-#             await transient_shared_rdm.cursor().cursor_index(),
-#             rdm_api.Err,
-#         )
-#         assert not isinstance(
-#             await transient_rdm.cursor().cursor_index(),
-#             rdm_api.Err,
-#         )
-#         async with loadable_rdm.sess() as rdm:
-#             assert not isinstance(
-#                 await rdm.cursor().cursor_index(),
-#                 rdm_api.Err,
-#             )
-
-#     @classmethod
-#     def test_constants_ok(cls) -> None:
-#         for nm, val in cls.CONSTANTS().items():
-#             assert val is not None, f"{cls.__name__}.{nm} is None"
+@pytest.mark.asyncio(loop_scope="class")
+class Test_RDM_Tests(RDM_Tests):
+    @staticmethod
+    async def test_fixtures_ok(
+        transient_shared_rdm: AsyncRocqDocManager,
+        transient_rdm: AsyncRocqDocManager,
+        loadable_rdm: AsyncRocqDocManager,
+    ) -> None:
+        assert not isinstance(
+            await transient_shared_rdm.cursor().cursor_index(),
+            rdm_api.Err,
+        )
+        assert not isinstance(
+            await transient_rdm.cursor().cursor_index(),
+            rdm_api.Err,
+        )
+        async with loadable_rdm.sess() as rdm:
+            assert not isinstance(
+                await rdm.cursor().cursor_index(),
+                rdm_api.Err,
+            )
