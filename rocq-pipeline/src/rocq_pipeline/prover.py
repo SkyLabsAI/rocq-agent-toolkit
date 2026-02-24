dfb952ba10bbbc5c95d75985dae0b6b541cf84ec
Jasper Haag <jasper@skylabs-ai.com>
2026-02-24 16:48:21 -0500

    refactor(rocq-pipeline/prover.py): properly handle partial completion of admitted proofs

--- rocq-pipeline/src/rocq_pipeline/prover.py
+++ rocq-pipeline/src/rocq_pipeline/prover.py
@@ -6,21 +6,28 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 from rocq_doc_manager import RocqCursor, rc_sess
+from rocq_doc_manager import rocq_doc_manager_api as rdm_api
 from rocq_dune_util import DuneError, rocq_args_for
 
 from rocq_pipeline.agent import AgentBuilder
 from rocq_pipeline.agent.proof.auto import AutoAgent
 from rocq_pipeline.args_util import adapt_usage, split_args, valid_file
 
+# QoL:
+# insert leading newline if proof doesn't start with one
 
 # TODOs:
-# 1) properly handle `admit`s in addition to `Admitted`
-# 2) parallel tasks:
+# 1) proof formatter
+# 2) properly handle `admit`s in addition to `Admitted`
+# 3) properly handle nested proofs
+# 4) parallel tasks:
 #    - utilize `materialize` and/or `clone(materialize=True)`
 #    - setup a `ThreadPoolExecutor` & use futures to manage interactions
 
 
-# Note: this will ignore `admit`s
+# Notes:
+# - this will ignore `admit`s
+# - this will not properly handle whitespace between "Admitted" and period
 def is_admitted(text: str, kind: str) -> bool:
     return kind == "command" and text == "Admitted."
 
@@ -40,26 +47,29 @@ async def run_proving_agent(
         output: the Path where the result of the interaction should be committed
         no_partial (optional): whether or not the persist partial proof progress for each admitted proof task
     """
+    if not any(filter(await rc.doc_suffix(), lambda suffix_item: is_admitted(suffix_item.text))):
+        print("No Admitted proofs.")
+        return
+
     print(
         f"Running the proving agent; partial proofs {'discarded' if no_partial else 'retained'}."
     )
 
     # Note: we could add `clone: bool = False` to `RocqCursorProtocolAsync.sess`
     # and transform `(await rc.clone()).sess()` into `await rc.sess(clone=True)`
-    with (await rc.clone()).sess() as main_rc:
-        while await main_rc.goto_first_match(is_admitted):
-            await run_delegated_prover_on_admitted_proof_task(
-                agent_cls,
-                main_rc,
-                no_partial=no_partial,
-            )
+    while await rc.goto_first_match(is_admitted):
+        await run_delegated_prover_on_admitted_proof_task(
+            agent_cls,
+            rc,
+            no_partial=no_partial,
+        )
 
-    await main_rc.commit(str(output), include_suffix=True)
+    await rc.commit(str(output), include_suffix=True)
 
 
 async def run_delegated_prover_on_admitted_proof_task(
     agent_cls: AgentBuilder,
-    main_rc: RocqCursor,
+    rc: RocqCursor,
     *,
     no_partial: bool = False,
 ) -> None:
@@ -67,38 +77,105 @@ async def run_delegated_prover_on_admitted_proof_task(
 
     Arguments:
         agent_cls: the AgentBuilder used to construct an Agent instance for the admitted proof task
-        main_rc: the (shared) RocqCursor to use for the admitted proof task
+        rc: the (shared) RocqCursor to use for the admitted proof task
         no_partial (optional): whether or not the persist partial proof progress for the admitted proof task
     """
-    await print_admitted_proof_task(main_rc)
+    await print_admitted_proof_task(rc)
 
-    with (await main_rc.clone()).sess() as local_rc:
+    # maybe not necessary
+    async with (await rc.clone(materialize=True)).sess() as local_rc:
+        await local_rc.clear_suffix()
+        existing_prefix = await local_rc.doc_prefix()
         task_result = await agent_cls().run(rc=local_rc)
 
-        should_persist = task_result.success or
+        async def agent_invariant_violated(message: str) -> None:
+            print(message)
+            # Step over `Admitted`
+            await rc.run_step()
+            pass
 
-        # NOTE: the pattern of switching to the other cursor is not great
-        # here, we should really copy the contents into the document.
-        # Doing this will enable some parallelization if we want.
+        # Insert partial progress if the proof is completed, or upon client request
+        if task_result.success or (not no_partial):
+            extended_prefix = await local_rc.doc_prefix()
+            expected_overlapping_prefix = extended_prefix[:len(existing_prefix)]
+            if existing_prefix != expected_overlapping_prefix:
+                # TODO: pretty-print diff of prefix text
+                return await agent_invariant_violated(
+                    "\n".join(
+                        [
+                            "Agent.prove invariant violated; prefix modified:",
+                            f"\t- expected prefix: {existing_prefix}",
+                            f"\t- actual prefix: {expected_overlapping_prefix}",
+                        ]
+                    )
+                )
+
+            extension = extended_prefix[len(existing_prefix):]
+
+            for prefix_item in extension:
+                # What happens if agents try inserting ghost commands? Either:
+                # - skip them
+                # - insert them as a comment
+                if prefix_item.kind == "blanks":
+                    insert_result = await rc.insert_blanks(text=prefix_item.text)
+                else:
+                    insert_result = await rc.insert_command(text=prefix_item.text, blanks=None)
+
+                if isinstance(insert_result, rdm_api.Err):
+
+                    ####################################
+                    # TODO: comment block with remaining
+                    ####################################
+
+                    return await agent_invariant_violated(
+                        f"Agent.prove invariant violated; replaying proof script failed: {insert_result}"
+                    )
+
+            # TODO:
+            # 1) hoist this to a helper
+            # 2) write a few small test cases for the helper
+
+
+            # 1a) check if last non-blank command in prefix is a `Qed`
+            #
+            # Note: brittle because agents could insert "extra" commands after `Qed`
+            command_extension = [
+                cmd
+                for cmd in extension[::-1]
+                if cmd.kind == "command"
+            ]
+            if len(command_extension) != 0 and command_extension[0].text == "Qed.":
+                # Clear `Admitted.` from suffix if this code -- or the agent -- succeeded w/Qed
+                # TODO: maybe check that the first item in the suffix is actually `Admitted`?
+                await rc.clear_suffix(count=1)
+            else:
+                # 1b) else, try inserting Qed
+                try_qed_result = await rc.insert_command("Qed.")  # TODO: maybe use `blanks=None`
+                if not isinstance(try_qed_result, rdm_api.Err):
+                    # Clear `Admitted.` from suffix if this code -- or the agent -- succeeded w/Qed
+                    # MAYBE: clear until the next `Admitted`
+                    await rc.clear_suffix(count=1)
+                else:
+                    # Step over `Admitted`
+                    await rc.run_step()
+        else: # Step over `Admitted`
+            await rc.run_step()
+
+        # Print task_result and persistence
         if task_result.success:
             print(f"Agent succeeded: {task_result.message}")
-            await local_rc.clear_suffix(count=1)
-            await local_rc.insert_command("Qed.", blanks=None)
-            old_rc = main_rc
-            main_rc = local_rc
-            await old_rc.dispose()
+        elif no_partial:
+            print(f"Agent made partial progress, but ultimately failed: {task_result.message}")
         else:
             print(f"Agent failed: {task_result.message}")
-            await local_rc.dispose()
-            await main_rc.run_step()
 
 
-async def print_admitted_proof_task(main_rc: RocqCursor) -> None:
+async def print_admitted_proof_task(local_rc: RocqCursor) -> None:
     """Print the admitted proof task captured by the state of main_rc."""
 
-    goal = await main_rc.current_goal()
+    goal = await local_rc.current_goal()
     assert goal is not None
-    print(f"\nFound admit at index {await main_rc.cursor_index()}.")
+    print(f"\nFound admit at index {await local_rc.cursor_index()}.")
     for i, g in enumerate(goal.focused_goals):
         print(f"Goal {i}:{g.replace('\n', '\n  ')}")
 
