6a760fa37a9c25bd8ce7d8b0b718a454b09b74b2
Jasper Haag <jasper@skylabs-ai.com>
2026-02-24 15:33:04 -0500

    refactor(rocq-pipeline): decompose run_proving_agent and prepare to handle partial proof progress

--- rocq-pipeline/src/rocq_pipeline/prover.py
+++ rocq-pipeline/src/rocq_pipeline/prover.py
@@ -13,41 +13,94 @@ from rocq_pipeline.agent.proof.auto import AutoAgent
 from rocq_pipeline.args_util import adapt_usage, split_args, valid_file
 
 
+# TODOs:
+# 1) properly handle `admit`s in addition to `Admitted`
+# 2) parallel tasks:
+#    - utilize `materialize` and/or `clone(materialize=True)`
+#    - setup a `ThreadPoolExecutor` & use futures to manage interactions
+
+
+# Note: this will ignore `admit`s
 def is_admitted(text: str, kind: str) -> bool:
     return kind == "command" and text == "Admitted."
 
 
 async def run_proving_agent(
-    rc: RocqCursor, agent_cls: AgentBuilder, output: Path
+    rc: RocqCursor,
+    agent_cls: AgentBuilder,
+    output: Path,
+    *,
+    no_partial: bool = False,
 ) -> None:
-    main_rc = await rc.clone()
-    print("Running the proving agent.")
-    while await main_rc.goto_first_match(is_admitted):
-        print()
-        goal = await main_rc.current_goal()
-        assert goal is not None
-        print(f"Found admit at index {await main_rc.cursor_index()}.")
-        for i, g in enumerate(goal.focused_goals):
-            print(f"Goal {i}:{g.replace('\n', '\n  ')}")
-        agent = agent_cls()  # Build the actual agent to run
-        local_rc = await main_rc.clone()  # should materialize this
-        task_result = await agent.run(rc=local_rc)
+    """Run the proving agent, delegating admitted proof tasks to agent_cls.
+
+    Arguments:
+        rc: the RocqCursor to use
+        agent_cls: the AgentBuilder used to construct an Agent instance for each admitted proof task
+        output: the Path where the result of the interaction should be committed
+        no_partial (optional): whether or not the persist partial proof progress for each admitted proof task
+    """
+    print(
+        f"Running the proving agent; partial proofs {'discarded' if no_partial else 'retained'}."
+    )
+
+    # Note: we could add `clone: bool = False` to `RocqCursorProtocolAsync.sess`
+    # and transform `(await rc.clone()).sess()` into `await rc.sess(clone=True)`
+    with (await rc.clone()).sess() as main_rc:
+        while await main_rc.goto_first_match(is_admitted):
+            await run_delegated_prover_on_admitted_proof_task(
+                agent_cls,
+                main_rc,
+                no_partial=no_partial,
+            )
+
+    await main_rc.commit(str(output), include_suffix=True)
+
+
+async def run_delegated_prover_on_admitted_proof_task(
+    agent_cls: AgentBuilder,
+    main_rc: RocqCursor,
+    *,
+    no_partial: bool = False,
+) -> None:
+    """Use agent_cls to attempt the admitted proof task captured by the state of main_rc.
+
+    Arguments:
+        agent_cls: the AgentBuilder used to construct an Agent instance for the admitted proof task
+        main_rc: the (shared) RocqCursor to use for the admitted proof task
+        no_partial (optional): whether or not the persist partial proof progress for the admitted proof task
+    """
+    await print_admitted_proof_task(main_rc)
+
+    with (await main_rc.clone()).sess() as local_rc:
+        task_result = await agent_cls().run(rc=local_rc)
+
+        should_persist = task_result.success or
 
         # NOTE: the pattern of switching to the other cursor is not great
         # here, we should really copy the contents into the document.
         # Doing this will enable some parallelization if we want.
         if task_result.success:
-            print(f"Agent succeeded with {task_result.message}")
+            print(f"Agent succeeded: {task_result.message}")
             await local_rc.clear_suffix(count=1)
             await local_rc.insert_command("Qed.", blanks=None)
             old_rc = main_rc
             main_rc = local_rc
             await old_rc.dispose()
         else:
-            print(f"Agent failed with {task_result.message}")
+            print(f"Agent failed: {task_result.message}")
             await local_rc.dispose()
             await main_rc.run_step()
-    await main_rc.commit(str(output), include_suffix=True)
+
+
+async def print_admitted_proof_task(main_rc: RocqCursor) -> None:
+    """Print the admitted proof task captured by the state of main_rc."""
+
+    goal = await main_rc.current_goal()
+    assert goal is not None
+    print(f"\nFound admit at index {await main_rc.cursor_index()}.")
+    for i, g in enumerate(goal.focused_goals):
+        print(f"Goal {i}:{g.replace('\n', '\n  ')}")
 
 
 def agent_main(agent_builder: AgentBuilder) -> int:
@@ -69,6 +122,11 @@ def agent_main(agent_builder: AgentBuilder) -> int:
         metavar="OUTPUT",
         type=valid_file(check_creatable=True, allowed_suffixes=[".v"]),
         help="output file (default is the input file)",
+    )
+    parser.add_argument(
+        "--no-partial",
+        action="store_true",
+        help="discard partial proofs",
     )
     adapt_usage(parser, "agent")
 
@@ -100,7 +158,12 @@ def agent_main(agent_builder: AgentBuilder) -> int:
                 dune=True,
                 load_file=True,
             ) as rc:
-                await run_proving_agent(rc, agent_builder, output)
+                await run_proving_agent(
+                    rc,
+                    agent_builder,
+                    output,
+                    no_partial=args.no_partial,
+                )
 
         asyncio.run(_run())
         return 0
