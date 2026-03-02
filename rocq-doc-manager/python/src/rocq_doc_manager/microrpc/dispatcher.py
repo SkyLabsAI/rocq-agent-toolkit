b59ed27a60dae5487a6dc47cffb9b78a7819b585
Gregory Malecha <gregory@skylabs-ai.com>
2026-03-01 22:01:31 -0500

    Migrating the protocol to jsonrpc2.0 and trying to consolidate the logic.

--- rocq-doc-manager/python/src/rocq_doc_manager/microrpc/dispatcher.py
+++ rocq-doc-manager/python/src/rocq_doc_manager/microrpc/dispatcher.py
@@ -1,5 +1,9 @@
 from typing import Any, Protocol, override
 
+from rocq_doc_manager.microrpc.deserialize import Decoder
+from rocq_doc_manager.microrpc.duplex import JsonValue
+from rocq_doc_manager.rocq_cursor_websocket import Encoder
+
 
 class Dispatcher(Protocol):
     """Dispatchers capture a JSON-RPC-style protocol.
@@ -7,9 +11,35 @@ class Dispatcher(Protocol):
     """
 
     async def dispatch(
-        self, method: str, args: list[Any], kwargs: dict[str, Any]
-    ) -> Any:
-        pass
+        self, method: str, params: JsonValue | None
+    ) -> tuple[bool, JsonValue]: ...
+
+
+class ClassDispatcher(Dispatcher):
+    def __init__(
+        self, target: object, *, whitelist: list[str], enc: Encoder, dec: Decoder
+    ) -> None:
+        super().__init__()
+        self._encoder = enc
+        self._decoder = dec
+        self._target = target
+        self._whitelist = whitelist
+
+    @override
+    async def dispatch(
+        self, method: str, params: JsonValue | None
+    ) -> tuple[bool, JsonValue]:
+        if method not in self._whitelist:
+            return (False, "Bad request")
+
+        meth = getattr(self._target, method)
+        # TODO: parse the arguments using the decoder
+        args: list[Any] = []
+        kwargs: dict[str, Any] = {}
+        try:
+            return (True, self._encoder.encode(meth(*args, **kwargs)))
+        except Exception as err:
+            return (False, self._encoder.encode(err))
 
 
 class NamespaceDispatcher(Dispatcher):
@@ -19,19 +49,29 @@ class NamespaceDispatcher(Dispatcher):
 
     _dispatchers: dict[str, Dispatcher]
 
-    def __init__(self, dispatchers: dict[str, Dispatcher]):
+    def __init__(
+        self, dispatchers: dict[str, Dispatcher], *, default: Dispatcher | None = None
+    ):
         """dispatchers is a mapping from namespace to the underlying Dispatcher."""
         self._dispatchers = dispatchers
+        self._default = default
 
     @override
     async def dispatch(
-        self, method: str, args: list[Any], kwargs: dict[str, Any]
-    ) -> Any:
+        self, method: str, params: JsonValue | None
+    ) -> tuple[bool, JsonValue]:
+        ns_method = None
         try:
             [ns, ns_method] = method.split("/", 1)
-        except ValueError as e:
+            dispatcher: Dispatcher | None = self._dispatchers[ns]
+        except ValueError:
             # Since this is communicated back to the caller, it may
             # not make sense to propagate e
-            raise ValueError(f"Unsupported method: '{method}'") from e
-        if ns in self._dispatchers:
-            return await self._dispatchers[ns].dispatch(ns_method, args, kwargs)
+            dispatcher = None
+        except IndexError:
+            dispatcher = None
+        if dispatcher and ns_method:
+            return await dispatcher.dispatch(ns_method, params)
+        elif self._default:
+            return await self._default.dispatch(method, params)
+        return (False, f"Unsupported method: '{method}'")
