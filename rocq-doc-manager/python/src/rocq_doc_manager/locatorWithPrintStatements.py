c344e286340f54981c6a7b215de779c1d7fe7fdb
Lennart Beringer <lennart.beringer@skylabs-ai.com>
2026-03-08 21:30:17 -0400

    Added a temporary file tracing the control flow of locator.goto

---
+++ rocq-doc-manager/python/src/rocq_doc_manager/locatorWithPrintStatements.py
@@ -0,0 +1,233 @@
+from __future__ import annotations
+
+import logging
+import re
+from collections.abc import Callable
+from typing import override
+from warnings import deprecated
+
+from rocq_doc_manager import RocqCursor
+from rocq_doc_manager import rocq_doc_manager_api as rdm_api
+
+logger = logging.getLogger(__name__)
+
+
+class Locator:
+    """A Locator refers to search for a line within a Rocq document.
+
+    Beyond __call__, implementers must override '__str__'."""
+
+    @deprecated("use `go_to`")
+    async def __call__(self, rc: RocqCursor, *, next: bool = False) -> bool:
+        return await self.go_to(rc, next=next)
+
+    async def go_to(self, rc: RocqCursor, *, next: bool = False) -> bool:
+        """Move the cursor to the line identified by the Locator.
+
+        If `next` is True, then the search occurs **forward** from the
+        current position of the RocqCursor.
+        """
+        return False
+
+    @override
+    def __eq__(self, other: object) -> bool:
+        if type(other) is type(self):
+            return str(self) == str(other)
+        return False
+
+
+class LocatorParser:
+    parsers: list[Callable[[str], Locator]] = []
+
+    @staticmethod
+    def register_parser(parser: Callable[[str], Locator]) -> None:
+        LocatorParser.parsers.append(parser)
+
+    @staticmethod
+    def parse(s: str) -> Locator:
+        for parser in LocatorParser.parsers:
+            try:
+                return parser(s)
+            except ValueError:
+                continue
+        raise ValueError(f"Failed to parse locator from `{s}`")
+
+
+class FirstAdmit(Locator):
+    """Find the first (or nth) use of the `admit` tactic."""
+
+    def __init__(self, index: int = 0) -> None:
+        self._index = index
+
+    def __str__(self) -> str:
+        if self._index == 0:
+            return "admit"
+        return f"admit({self._index})"
+
+    @override
+    async def go_to(self, rc: RocqCursor, *, next: bool = False) -> bool:
+        def is_admit(
+            text: str,
+            kind: str,
+        ) -> bool:
+            return kind == "command" and text.startswith("admit")
+
+        return await rc.goto_first_match(
+            is_admit, skip=self._index, include_prefix=not next
+        )
+
+    PTRN_PARSE = re.compile(r"admit(\([0-9]+\))?")
+
+    @staticmethod
+    def parse(s: str) -> FirstAdmit:
+        if not (mtch := FirstAdmit.PTRN_PARSE.match(s)):
+            raise ValueError(f"Failed to parse FirstAdmit from '{s}'")
+        if mtch.group(1):
+            try:
+                index: int = int(mtch.group(1)[1:-1])
+            except BaseException as err:
+                raise ValueError(
+                    f"Failed to parse integer from '{mtch.group(1)}'."
+                ) from err
+            return FirstAdmit(index)
+        return FirstAdmit()
+
+
+LocatorParser.register_parser(FirstAdmit.parse)
+
+
+class FirstLemma(Locator):
+    def __str__(self) -> str:
+        which = f"({self._index})" if self._index != 0 else ""
+        if self._style is None:
+            return f"lemma:{self._name}{which}"
+        else:
+            return f"{self._style}:{self._name}{which}"
+
+    def __init__(self, lemma_name: str, style: str | None = None, index: int = 0):
+        self._name = lemma_name
+        self._style = style
+        self._index = index
+
+    @override
+    async def go_to(self, rc: RocqCursor, *, next: bool = False) -> bool:
+        if self._style is None:
+            prefix = "Lemma|Theorem"
+        else:
+            prefix = self._style
+
+        mtch = re.compile(f"({prefix})\\s+{self._name}[^0-9a-zA-Z_']")
+
+        def is_lemma(
+            text: str,
+            kind: str,
+        ) -> bool:
+            return kind == "command" and mtch.match(text) is not None
+
+        is_proof = False
+        print("Calling first_match")
+        if await rc.goto_first_match(
+            is_lemma, step_over_match=True, skip=self._index, include_prefix=not next
+        ):
+            suffixes = await rc.doc_suffix()
+            print(f"  Found first match. There are {str(len(suffixes))}, the first one being '{suffixes[0].text}'. And is_proof = {str(is_proof)}.")
+            for cmd in suffixes:
+                print(f"  Visiting suffix '{cmd.text}'. Here, is_proof = {str(is_proof)}.")
+                if cmd.kind != "blanks":
+                    print(f"    Not blanck")
+                    is_proof = False
+                if cmd.kind != "command" or (
+                    cmd.kind == "command" and cmd.text.startswith("Proof")
+                ):
+                    print(f"    Running step. is_proof = {str(is_proof)}.")
+                    run_step_reply = await rc.run_step()
+                    if isinstance(run_step_reply, rdm_api.Err):
+                        logger.warning(f"RocqCursor.run_step failed: {run_step_reply}")
+                        return False
+                    print(f"    Step succeeded. is_proof = {str(is_proof)}.")
+                    if cmd.text.startswith("Proof"):
+                        print(f"    Setting is_proof.")
+                        is_proof = True
+                else:
+                    print(f"    Returning True.")
+                    return True
+            print(f"    Returning inner isproof: {str(is_proof)}.")
+            return is_proof
+        print(f"    Returning outer isproof: {str(is_proof)}.")
+        return is_proof
+
+    @staticmethod
+    def parse(s: str) -> FirstLemma:
+        def get_index(s: str) -> tuple[str, int]:
+            ptrn: re.Pattern = re.compile(r"(.+)\(([0-9]+)\)")
+            mtch = ptrn.match(s)
+            if mtch is None:
+                return (s, 0)
+            else:
+                return mtch.group(1), int(mtch.group(2))
+
+        for kw in ["Lemma", "Theorem"]:
+            kw_colon = kw + ":"
+            if s.startswith(kw_colon):
+                nm, index = get_index(s[len(kw_colon) :])
+                return FirstLemma(nm, kw, index=index)
+
+        if s.startswith("lemma:"):  # Backwards compatibility
+            logger.warning(
+                " ".join(
+                    [
+                        '"lemma:" locator is deprecated,',
+                        'use "Theorem:" or "Lemma:":',
+                        s,
+                    ]
+                )
+            )
+            return FirstLemma(s[len("lemma:") :])
+
+        raise ValueError(f"Failed to parse FirstLemma from '{s}'")
+
+
+LocatorParser.register_parser(FirstLemma.parse)
+
+
+class CommentMarkerLocator(Locator):
+    """Locates a comment that contains the given string."""
+
+    PREFIX = "comment_marker"
+    PTRN_PARSE = re.compile(r"comment_marker(\([0-9]+\))?:")
+
+    def __init__(self, marker: str, index: int = 0):
+        self._marker = marker
+        self._index = index
+
+    def __str__(self) -> str:
+        if self._index:
+            return f"{CommentMarkerLocator.PREFIX}({self._index}){self._marker}"
+        return f"{CommentMarkerLocator.PREFIX}{self._marker}"
+
+    @override
+    async def go_to(self, rc: RocqCursor, *, next: bool = False) -> bool:
+        def is_marker_comment(
+            text: str,
+            kind: str,
+        ) -> bool:
+            return kind == "blanks" and self._marker in text
+
+        return await rc.goto_first_match(is_marker_comment, include_prefix=not next)
+
+    @staticmethod
+    def parse(s: str) -> CommentMarkerLocator:
+        if not (mtch := CommentMarkerLocator.PTRN_PARSE.match(s)):
+            raise ValueError(f"Failed to parse 'CommentMarkerLocator' from {s}")
+        marker = s[len(mtch.group(0)) :]
+        if mtch.group(1):
+            try:
+                return CommentMarkerLocator(marker, int(mtch.group(1)[1:-1]))
+            except ValueError as err:
+                raise ValueError(
+                    f"Failed to parse 'CommentMarkerLocator' from {s}"
+                ) from err
+        return CommentMarkerLocator(marker, 0)
+
+
+LocatorParser.register_parser(CommentMarkerLocator.parse)
