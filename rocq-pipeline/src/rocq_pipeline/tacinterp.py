bd06d0c2fef8a5a74df8809317dfbef320a66f94
Gregory Malecha <gregory@skylabs-ai.com>
2026-03-24 14:43:34 -0400

    Implement some of the variants on then.

--- rocq-pipeline/src/rocq_pipeline/tacinterp.py
+++ rocq-pipeline/src/rocq_pipeline/tacinterp.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 import sys
-from collections.abc import Awaitable, Callable
+from collections.abc import Awaitable, Callable, Sequence
 from typing import cast
 
 from rocq_doc_manager import RocqCursor, rc_sess
@@ -49,19 +49,34 @@ async def run_on_each(
     count.
     """
     print(f" > run_atom({tactic}, {goals})")
-    first_goal, count = goals
+    _, count = goals
     if multi_goal:
         raise NotImplementedError()
     else:
-        # If this is a single-goal tactic, then we run the tactic on each
-        # goal.
-        base = first_goal
-        current_goal = await rc.current_goal()
-        if current_goal is None:
-            raise NotImplementedError()
+        return await run_on_goals(rc, [run_tactic] * count, goals=goals)
+
+
+async def run_on_goals(
+    rc: RocqCursor,
+    tacs: Sequence[Callable[[RocqCursor, int], Awaitable[RunCommandResult]] | None],
+    goals: tuple[int, int] = (0, 1),
+) -> int:
+    first_goal, count = goals
+    if len(tacs) != count:
+        raise LtacFail()
+
+    # If this is a single-goal tactic, then we run the tactic on each
+    # goal.
+    base = first_goal
+    current_goal = await rc.current_goal()
+    if current_goal is None:
+        raise NotImplementedError()
+    else:
+        focused_goals = current_goal.focused_goals
+    for i, run_tactic in enumerate(tacs):
+        if run_tactic is None:
+            base += 1
         else:
-            focused_goals = current_goal.focused_goals
-        for i in range(0, count):
             result = await run_tactic(rc, base)
             if isinstance(result, rdm_api.Err):
                 raise LtacFail(result)
@@ -69,7 +84,8 @@ async def run_on_each(
                 # diff current_goal[base+1] with result[base:]
                 pf_state = result
                 if pf_state is None:
-                    raise NotImplementedError()
+                    assert i == count - 1
+                    return 0
                 elif base + 1 < len(focused_goals):
                     try:
                         base = pf_state.focused_goals[base:].index(
@@ -82,12 +98,22 @@ async def run_on_each(
                 else:
                     assert i == count - 1
                     return len(pf_state.focused_goals) - first_goal
-        return base - first_goal
+    return base - first_goal
+
+
+def decomp_run_lam(
+    tactic: TacticAST,
+) -> Callable[[RocqCursor, int], Awaitable[RunCommandResult]]:
+    async def run(rc: RocqCursor, goal: int) -> RunCommandResult:
+        _count = await decomp_run(rc, tactic, goals=(goal, 1))
+        return await rc.current_goal()
+
+    return run
 
 
 async def decomp_run(
     rc: RocqCursor, tactic: TacticAST, *, goals: tuple[int, int]
-) -> tuple[int, int]:
+) -> int:
     print(f" > decomp_run({json.dumps(tactic)}, {goals})")
     first_goal, count = goals
     match tactic[0]:
@@ -95,15 +121,48 @@ async def decomp_run(
             tac = tactic[1]
             assert isinstance(tac, str)
             result = await run_on_each(rc, cast(str, tac), run_tac(tac), goals=goals)
-            return (first_goal, result)
+            return result
         case "Then":
             [_, tac1, tac2] = tactic
             # assert isinstance(tac1, TacticAST)
             # assert isinstance(tac2, TacticAST)
             new_goals = await decomp_run(rc, cast(TacticAST, tac1), goals=goals)
-            return await decomp_run(rc, cast(TacticAST, tac2), goals=new_goals)
+            return await decomp_run(
+                rc, cast(TacticAST, tac2), goals=(first_goal, new_goals)
+            )
+        case "Thens":
+            new_goals = await decomp_run(rc, cast(TacticAST, tactic[1]), goals=goals)
+            if new_goals != len(tactic[2]):
+                raise LtacFail()
+            return await run_on_goals(
+                rc,
+                [decomp_run_lam(cast(TacticAST, tac)) for tac in tactic[2]],
+                goals=(first_goal, new_goals),
+            )
+        case "Thens3":
+            [_, tac, before, middle, after] = tactic
+            count = await decomp_run(rc, cast(TacticAST, tac), goals=goals)
+            return await decomp_run(
+                rc, ["ExtendTac", before, middle, after], goals=(first_goal, count)
+            )
+        case "ExtendTac":
+            [_, before, middle, after] = tactic
+            if count < len(before) + len(after):
+                raise LtacFail()
+
+            tacs: Sequence[
+                Callable[[RocqCursor, int], Awaitable[RunCommandResult]] | None
+            ] = (
+                [decomp_run_lam(cast(TacticAST, tac)) for tac in before]
+                + (
+                    [decomp_run_lam(cast(TacticAST, middle))]
+                    * (count - len(before) - len(after))
+                )
+                + [decomp_run_lam(cast(TacticAST, tac)) for tac in after]
+            )
+            return await run_on_goals(rc, tacs, goals=(first_goal, count))
         case "Idtac":
-            return goals
+            return count
         case "Fail":
             if tactic[1] not in ["fail", "fail 0"]:
                 raise NotImplementedError(tactic[1])
@@ -127,8 +186,7 @@ async def decomp_run(
                         continue
                 raise LtacFail()
 
-            new_count = await run_on_each(rc, "<first>", run_first, goals=goals)
-            return (first_goal, new_count)
+            return await run_on_each(rc, "<first>", run_first, goals=goals)
 
         case "Solve":
 
@@ -137,12 +195,12 @@ async def decomp_run(
                 for t in cast(list[TacticAST], tactic[1:]):
                     try:
                         async with (await rc.clone()).sess() as rc_attempt:
-                            _, count = await decomp_run(rc_attempt, t, goals=(goal, 1))
+                            count = await decomp_run(rc_attempt, t, goals=(goal, 1))
                             if count > 0:
                                 raise LtacFail()
 
                         print(">>>>>>>>>>>>>> REPLICATE")
-                        _, count = await decomp_run(rc, t, goals=(goal, 1))
+                        count = await decomp_run(rc, t, goals=(goal, 1))
                         print("<<<<<<<<<<<<<< REPLICATE")
                         assert count == 0
                         return await rc.current_goal()
@@ -150,8 +208,7 @@ async def decomp_run(
                         continue
                 raise LtacFail()
 
-            new_count = await run_on_each(rc, "<solve>", run_solve, goals=goals)
-            return (first_goal, new_count)
+            return await run_on_each(rc, "<solve>", run_solve, goals=goals)
 
         case "Try":
 
@@ -168,8 +225,7 @@ async def decomp_run(
                     pass
                 return await rc.current_goal()
 
-            new_count = await run_on_each(rc, "<try>", run_try, goals=goals)
-            return (first_goal, new_count)
+            return await run_on_each(rc, "<try>", run_try, goals=goals)
 
         case ctor:
             raise NotImplementedError(ctor)
@@ -200,8 +256,8 @@ async def amain(file: str, locator: str) -> int:
                 print("query failed!")
                 raise Exception("Oops")
             explanation = json.loads(explanation.feedback_messages[0].text)
-            (start, count) = await decomp_run(rc, explanation, goals=(0, 1))
-            print(f"> ({start}, {count})")
+            count = await decomp_run(rc, explanation, goals=(0, 1))
+            print(f"> ({count})")
             print("")
     return 0
 
